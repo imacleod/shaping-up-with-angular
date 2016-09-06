d9e87b561fba7816fc23c4e377b5d2d567c0907d
Ian MacLeod <imacleod@users.noreply.github.com>
2016-09-06 15:36:12 -0700

    Finished section 1 (added multiple products to store).

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,21 @@
 (function(){
-	var app = angular.module('store', []);
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-	}
+	var app = angular.module('gemStore', []);
+	var gems = [
+		{
+			canPurchase: true,
+			description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+			name: 'Dodecahedron',
+			price: 2.95
+		},
+		{
+			canPurchase: false,
+			description: 'Quite the gem, this one!',
+			name: 'Pentagonal Gem',
+			price: 5.95
+		}
+	];
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 })();
