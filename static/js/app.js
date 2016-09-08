f785f9d61dbef560642978391b6c69e4732acb9a
Ian MacLeod <imacleod@users.noreply.github.com>
2016-09-08 11:41:15 -0700

    Changed product display to list.

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@
 			canPurchase: true,
 			description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
 			name: 'Dodecahedron',
-			price: 2.95
+			price: 2
 		},
 		{
 			canPurchase: false,
